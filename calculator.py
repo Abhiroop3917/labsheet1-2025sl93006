c376c0e4fce2d0f7d8d8cce7a2598100aebc33c5
Abhiroop3917 <abhisingamshetty@gmail.com>
2026-03-31 19:00:36 +0530

    introduced bug

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def add(a,b):
-    return a+b
+    return a-b
 
 def multiply(a,b):
     return a*b
@@ -12,3 +12,6 @@ def divide(a,b):
         return None
     return a/b
 
+if __name__ == "__main__":
+    assert add(2, 3) == 5
+    print("All tests passed")
